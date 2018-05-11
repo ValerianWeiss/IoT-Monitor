81cdd839c0be9978bab56a772279d14e139559d8
ValerianWeiss <valerian97@web.de>
2018-05-11 17:22:14 +0200

    Modified Dockerfiles

--- vue-webapp/src/main.ts
+++ vue-webapp/src/main.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import app from './app.vue';
 import router from './router';
-import store from './store'
+import store from './store';
 
 new Vue({
   el: '#app',
--- vue-webapp/src/router.ts
+++ vue-webapp/src/router.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import login from './components/login.vue';
-import home from './components/home.vue'
+import home from './components/home.vue';
 
 Vue.use(VueRouter);
 
