7f78f89433bf52642c62114afda1cda225da34a2
ValerianWeiss <valerian97@web.de>
2018-07-16 23:14:04 +0200

    test state

--- vue-backend-resources/src/main/java/com/vuebackend/controllers/EndpointController.java
+++ vue-backend-resources/src/main/java/com/vuebackend/controllers/EndpointController.java
@@ -23,7 +23,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
