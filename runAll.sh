c87ca99ea4370772f4570f1f4313332f7fe98fe9
ValerianWeiss <valerian97@web.de>
2018-07-31 14:28:06 +0200

    ajusted runAll.sh

--- runAll.sh
+++ runAll.sh
@@ -2,26 +2,34 @@
 
 scriptDir=$(pwd)
 
+echo %scriptDir%
 
 cd "./vue-backend-registry/build/libs"
-java -jar vue-backend-registry-0.0.1.jar &
+terminal "java -jar vue-backend-registry-0.0.1.jar"
 
 cd $scriptDir
 
 cd "./vue-backend-auth/build/libs"
-java -jar vue-backend-auth-0.0.1.jar &
+terminal "java -jar vue-backend-auth-0.0.1.jar"
 
 cd $scriptDir
 
 cd "./vue-backend-resources/build/libs"
-java -jar vue-backend-recources-0.0.1.jar &
+terminal "java -jar vue-backend-recources-0.0.1.jar"
 
 cd $scriptDir
 
 cd "./vue-backend-websockets/build/libs"
-java -jar vue-backend-websockets-0.0.1.jar &
+terminal "java -jar vue-backend-websockets-0.0.1.jar"
+
+cd $scriptDir
+
+cd "./vue-backend-gateway/build/libs"
+terminal "java -jar vue-backend-gateway-0.0.1.jar"
 
 cd $scriptDir
 
 cd "./vue-webapp"
-npm run dev
+terminal "npm run dev"
+
+cd $scriptDir
